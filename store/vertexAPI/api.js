ba3e0ec0d1bc8beca658fd03c245970316ec99da
Crowdbotics <engineering@crowdbotics.com>
2024-07-07 11:47:17 +0000

    Update store.

--- store/vertexAPI/api.js
+++ store/vertexAPI/api.js
@@ -12,6 +12,24 @@ function api_v1_login_create(payload) {
 function api_v1_signup_create(payload) {
   return vertexAPI.post(`/api/v1/signup/`, payload)
 }
+function modules_files_uploads_list(payload) {
+  return vertexAPI.get(`/modules/files/uploads/`)
+}
+function modules_files_uploads_create(payload) {
+  return vertexAPI.post(`/modules/files/uploads/`, payload)
+}
+function modules_files_uploads_retrieve(payload) {
+  return vertexAPI.get(`/modules/files/uploads/${payload.id}/`)
+}
+function modules_files_uploads_update(payload) {
+  return vertexAPI.put(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_partial_update(payload) {
+  return vertexAPI.patch(`/modules/files/uploads/${payload.id}/`, payload)
+}
+function modules_files_uploads_destroy(payload) {
+  return vertexAPI.delete(`/modules/files/uploads/${payload.id}/`)
+}
 function rest_auth_login_create(payload) {
   return vertexAPI.post(`/rest-auth/login/`, payload)
 }
@@ -49,6 +67,12 @@ export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
   api_v1_signup_create,
+  modules_files_uploads_list,
+  modules_files_uploads_create,
+  modules_files_uploads_retrieve,
+  modules_files_uploads_update,
+  modules_files_uploads_partial_update,
+  modules_files_uploads_destroy,
   rest_auth_login_create,
   rest_auth_logout_create,
   rest_auth_password_change_create,
